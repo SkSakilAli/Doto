ea76454dfdf5f34def2401e73b5b45f550cc004e
Sk Sakil Ali <187862446+SkSakilAli@users.noreply.github.com>
2025-08-02 19:12:50 +0530

    Add Functionality - Marks as Complete and Dynamic Header

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const copy = document.querySelectorAll(".Task")[0].cloneNode(true);
 let numberOfTask = 1;
 let taskNew;
 let taskNumberNew;
-let CompletedTask;
+let CompletedTask = 0;
 let tasks ={
     0: document.getElementsByClassName(".Task")[0],
 
@@ -12,6 +12,7 @@ let tasks ={
 tasks[numberOfTask] = copy.cloneNode(true);
 child.appendChild(tasks[numberOfTask]);
 tasks[numberOfTask].setAttribute("Id", numberOfTask);
+tasks[numberOfTask].querySelectorAll(".buttonGreen")[0].setAttribute("ID","btg"+numberOfTask);
 taskNew = document.getElementById(numberOfTask);
 taskNumberNew = taskNew.querySelectorAll(".TaskNumber")[0];
 taskNumberNew.textContent = numberOfTask + 1;
@@ -26,11 +27,42 @@ reset: ()=>{
     child.removeChild(document.getElementById(i));
     document.querySelectorAll(".Task .secondaryText")[0].textContent = "Add your Task Here"
     if(i==1){ numberOfTask = 1; }
+
 }
 },
+    markComplete: (event)=>{
+        let id = event.target.offsetParent.id;
+        if(id=="first"){
+            id = event.target.id;
+        }
+        let divId = id[id.length-1];
+        document.getElementById(divId).querySelectorAll(".secondaryText")[0].style ="color: #9B9B9B; text-decoration: line-through; ";
+
+        document.getElementById(divId).querySelectorAll(".TaskNumber")[0].style ="color: #9B9B9B; text-decoration: line-through;  ";
+
+        document.getElementById(divId).querySelectorAll(".buttonGreen")[0].setAttribute("disabled", "true");
+ document.getElementById(divId).querySelectorAll(".buttonGreen")[0].style="background-color:#B1B1B1;";
+ document.getElementById(divId).querySelectorAll(".buttonGreen span")[0].textContent = " Completed :)";
+CompletedTask += 1;
+        switch(CompletedTask){
+            case 1: document.getElementById("heading").textContent = "Hell Yeah!! First Task Completed. Don't lose the pace";
+                break;
+            case 2: document.getElementById("heading").textContent = "Let's Goooo! Task two done";
+                break;
+            case 3: document.getElementById("heading").textContent = "Yay! Hatrick it is";
+                break;
+            case 4: document.getElementById("heading").textContent = "You are Unstoppable!";
+                break;
+            case numberOfTask: document.getElementById("heading").textContent = "Congratulations, You did it! You have Completed all tasks.";
+                break;
+        }
+document.getElementById("tpHeaderSub").textContent = (CompletedTask/numberOfTask)*100 + "%  Task Completed "
+
+    }
 
 }
 tasks.addbtn.addEventListener("click", tasks.add);
 tasks.resetButton.addEventListener("click", tasks.reset);
 
+document.getElementById("todoHeaderHead").textContent= new Date().toDateString();
 
