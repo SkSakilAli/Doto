8addab25b2356df201c1c3057119e2cb247dbea9
Sk Sakil Ali <187862446+SkSakilAli@users.noreply.github.com>
2025-08-02 15:56:49 +0530

    Add Functionality - Reset Tasks

--- script.js
+++ script.js
@@ -17,8 +17,20 @@ taskNumberNew = taskNew.querySelectorAll(".TaskNumber")[0];
 taskNumberNew.textContent = numberOfTask + 1;
 numberOfTask++;
 
+
+   },
+//Adding Reset
+    resetButton: document.getElementById("resetTask"),
+reset: ()=>{
+    for(let i = numberOfTask-1; i>=1; i--){
+    child.removeChild(document.getElementById(i));
+    document.querySelectorAll(".Task .secondaryText")[0].textContent = "Add your Task Here"
+    if(i==1){ numberOfTask = 1; }
 }
+},
+
 }
 tasks.addbtn.addEventListener("click", tasks.add);
+tasks.resetButton.addEventListener("click", tasks.reset);
 
 
